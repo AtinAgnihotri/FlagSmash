f5be3825a783e246ca3c43d6e684e3a098b9bab1
ATIN AGNIHOTRI <atin.agnihotri@paytmmoney.com>
2021-05-17 22:32:04 +0530

    Initial Commit

---
+++ FlagSmash/FlagSmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagSmashApp.swift
+//  FlagSmash
+//
+//  Created by Atin Agnihotri on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagSmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
